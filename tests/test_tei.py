65eece21f7abbd2856894f642b1cfa6ebf387d3c
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2025-06-23 21:54:25 +1000

    :zap: adding tests for csv to tsv with dates

--- tests/test_tei.py
+++ tests/test_tei.py
@@ -25,3 +25,22 @@ def test_csv_to_tei(tmp_path):
     assert '<listWit>\n\t\t\t\t\t<witness n="A" />' in output_data
 
 
+def test_csv_to_tei_dates(tmp_path):
+    output_file = tmp_path / "output-tei.xml"
+    result = runner.invoke(app, [
+        "csv-to-tei",
+        str(TEST_DATA/"demo-readings.csv"),
+        str(output_file),
+        "--dates",
+        str(TEST_DATA/"demo-dates.csv"),
+    ])
+    assert result.exit_code == 0
+    
+    assert output_file.exists()
+    output_data = output_file.read_text()
+    assert output_data.startswith('<TEI xmlns="http://www.tei-c.org/ns/1.0">\n\t<teiHeader>')
+    assert '<p>Derived from `demo-readings.csv`</p>' in output_data
+    assert '<rdg wit="B C D">1</rdg>' in output_data
+    assert '<listWit>\n\t\t\t\t\t<witness n="A">' in output_data
+    assert '<origDate notBefore="200" notAfter="299" />' in output_data
+
